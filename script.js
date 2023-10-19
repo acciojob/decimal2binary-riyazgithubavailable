a5d1880b250613e7773361281697d6560fbf9391
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2023-10-19 12:50:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let output = "";
+	while(num>=2){
+		let remainder=num%2;
+		output=remainder+output;
+		num=parseInt(num/2);
+	}
+  if(num===1){
+	  output=1+output;
+  }
+	console.log(output);
 }
 
 window.decimalToBinary = decimalToBinary;
